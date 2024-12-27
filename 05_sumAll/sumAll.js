81e319421e54d6cf3232686120a60e83b1b1153c
corwsan <191419896+corwsan@users.noreply.github.com>
2024-12-27 16:55:10 +1300

    Edit function to work if larger number is first

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
 const sumAll = function(num1, num2) {
+    if (num1 > num2) {
+        const tempNum = num1;
+        num1 = num2;
+        num2 = tempNum;
+    }
+
     let sum = num1 + num2
+    
+    //let timesToLoop = function (num1, num2) { return Math.abs(num1-num2);}
     for (let i = num1 + 1; i < num2; i++) {
         sum = sum + i;
     }
