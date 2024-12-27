326460c6edb20adcdf9a2b56d6ab96c61293c02f
corwsan <191419896+corwsan@users.noreply.github.com>
2024-12-27 16:12:18 +1300

    Implement function to pass test 1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = num1 + num2
+    for (let i = num1 + 1; i < num2; i++) {
+        sum = sum + i;
+    }
+    return sum
 };
 
 // Do not edit below this line
