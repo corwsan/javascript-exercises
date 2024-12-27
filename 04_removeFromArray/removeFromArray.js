6fceaf9288d558d76d6e72f98e2f562ea8241629
corwsan <191419896+corwsan@users.noreply.github.com>
2024-12-27 14:40:23 +1300

    Create function to pass first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, toRemove) {
+
+    const newArray = array.filter(checkFilter);
+
+        function checkFilter (item) {
+            return item != toRemove;
+        }
+
+    return newArray;
 };
 
 // Do not edit below this line
