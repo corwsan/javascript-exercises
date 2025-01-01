2b363960a308e859ac928d02b70e7c7a9918fc25
corwsan <191419896+corwsan@users.noreply.github.com>
2025-01-01 20:07:11 +1300

    Create function to pass all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+/*
+Take the string
+turn it into an array
+reverse it
+check each element one by one
+return false if one found to be different
+return true if all the same
+ignore punctuation
+*/
+
+
+const palindromes = function (string) {
+
+    let forwardString = string.toLowerCase().replace(/[,.!]/g,"").split(" ").join("");
+    let reverseString = forwardString.split("").reverse().join("")
+    return forwardString === reverseString;
 
 };
 
